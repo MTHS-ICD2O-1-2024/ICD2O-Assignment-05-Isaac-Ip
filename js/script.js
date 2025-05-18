5d31e5c0d1a37db30eacb9e1b5a60c8e5c77ffd1
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-18 02:39:36 +0000

    code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function performs simplified long division.
+ */
+function generate() {
+  const amountOfNumbers = parseInt(document.getElementById("amount-of-numbers").value);
+  document.getElementById("result").innerText = "";
+  let value1 = 0;
+  let value2 = 1;
+  let sum = 0;
+  // process
+  for (let i = 1; i <= amountOfNumbers; i++) {
+    document.getElementById("result").innerText += sum;
+    sum = (value1 + value2);
+    value1 = value2;
+    value2 = sum;
+    i = i + 1;
+    }
+  }
+
